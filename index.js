5110da0dc7197a3fe460534611a08be9fe80eb71
Michal Vlasák <michal.vlasak@ackee.cz>
2019-01-17 10:04:54 +0100

    Add rules different in airbnb config

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ module.exports = {
     'jsRules': true,
     'rules': {
         'align': [true, 'parameters'],
+        'max-line-length': false,
+        'member-ordering': {
+            'severity': 'warning',
+            'options': 'statics-first'
+        },
+        'no-angle-bracket-type-assertion': {
+            'severity': 'warning'
+        },
+        'no-namespace': [true, 'allow-declarations'],
+        'no-unused-variable': [true, {"ignore-pattern": "^_"}],
+        'object-curly-spacing': {
+            'severity': 'warning',
+            'options': ['always']
+        },
+        'ordered-imports': {
+            'severity': 'warning'
+        },
+        'semicolon': {
+            'severity': 'warning',
+            'options': ['always']
+        },
+        'ter-max-len': [true, 160, 4, {
+            'ignoreComments': true,
+            'ignoreUrls': true,
+            'ignorePattern': '^\\s*var\\s.+=\\s*require\\s*\\('
+        }],
         'trailing-comma': [true, {
             'multiline': {
                 'objects': 'always',
@@ -20,12 +46,6 @@ module.exports = {
                 'exports': 'ignore',
                 'functions': 'ignore'
             }
-        }],
-        'max-line-length': false,
-        'ter-max-len': [true, 160, 4, {
-            'ignoreComments': true,
-            'ignoreUrls': true,
-            'ignorePattern': '^\\s*var\\s.+=\\s*require\\s*\\('
         }],
         'variable-name': [true, 'check-format', 'allow-leading-underscore'],
     },
